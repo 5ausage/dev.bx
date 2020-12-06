a892fedb041a9f9c2f5829113676c34e5ed56e17
Ivan Galiguzov <i.galiguzov@yandex.ru>
2020-12-06 23:41:55 +0200

    Directory Reading function folder deleted

--- Directory reading function/TDD for Directory Reading.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function DirectoryReading_Testing($ExpectedResult)
-{
-	$Result = getDirectoryStatus("./test/", $Output);
-	if($Result === $ExpectedResult) return 'passed'; else return 'failed';
-}
