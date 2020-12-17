c7cda843fb6cd30c4ff4675a29819a16ff8cb9ac
Ivan Galiguzov <i.galiguzov@yandex.ru>
2020-12-17 23:26:43 +0200

    devtest

---
+++ Tasks from devtest/A7.php
@@ -0,0 +1,28 @@
+<?php
+
+echo 'Введите слова через пробел'.PHP_EOL;
+$a = trim(fgets(STDIN));
+
+echo 'Введите символ'.PHP_EOL;
+$s = trim(fgets(STDIN));
+
+echo 'Введите номер буквы слова'.PHP_EOL;
+$n = trim(fgets(STDIN));
+
+$str = explode(' ', $a);
+
+for ($i = 0; $i < count($str); $i++)
+{
+	if(strlen($str[$i]) >= $n)
+	{
+		$str[$i][$n - 1] = $s;
+	}
+	echo $str[$i].' ';
+}
+
+/*
+(A7) Дана строка, состоящая из слов, слова разделяются пробелами.
+Необходимо в каждом слове N-ю букву заменить на указанный символ.
+
+https://trycode.pw/c/6JVN4
+*/
