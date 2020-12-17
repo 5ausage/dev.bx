74b2d4564b5db01f7cb291029fcfa8d4e6b24fd2
Ivan Galiguzov <i.galiguzov@yandex.ru>
2020-12-17 23:14:21 +0200

    devtest

--- Tasks from devtest/A7.php
+++ Tasks from devtest/A7.php
@@ -1,21 +1,28 @@
 <?php
 
+echo 'Введите строку'.PHP_EOL;
+$a = trim(fgets(STDIN));
+
 echo 'Введите символ'.PHP_EOL;
 $s = trim(fgets(STDIN));
 
 echo 'Введите номер элемента строки'.PHP_EOL;
 $n = trim(fgets(STDIN));
 
-echo 'Введите строку'.PHP_EOL;
-$a = trim(fgets(STDIN));
+$str = explode(' ', $a);
 
-$str = str_split($a);
-
-$str[$n-1] = $s;
-
-for($i = 0; $i < count($str); $i++)
+for ($i = 0; $i < count($str); $i++)
 {
-	echo $str[$i];
+	if(strlen($str[$i]) >= $n)
+	{
+		$str[$i][$n - 1] = $s;
+	}
+	echo $str[$i].' ';
 }
 
-/*https://trycode.pw/c/6JVN4*/
+/*
+(A7) Дана строка, состоящая из слов, слова разделяются пробелами.
+Необходимо в каждом слове N-ю букву заменить на указанный символ.
+
+https://trycode.pw/c/6JVN4
+*/
