51df2122a6a1f2d8af6e44bfde213e4dd19c044b
Ivan Galiguzov <i.galiguzov@yandex.ru>
2020-12-23 08:14:54 +0200

    Mockup 2

---
+++ № 957/header.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="css/header_style.css">
+<header class="container-fluid">
+	<div class="row" style="height: 64px">
+		<div class="col text-left">
+			<div class="nav" style="height: 22px">
+				<a href="home.php" class="nav-link" style="height: 22px; width: 64px">Home</a>
+				<a href="productlist.php" class="nav-link" style="height: 22px; width: 64px">ProdList</a>
+				<a href="detail.php" class="nav-link" style="height: 22px; width: 64px">Detail</a>
+				<a href="login.php" class="nav-link" style="height: 22px; width: 64px">Login</a>
+			</div>
+		</div>
+		<div class="col center">
+			<div class="logo text-center" style="height: 29px">TRADE</div>
+		</div>
+
+		<div class="col text-right">
+			<a href="" class="btn" style="height: 40px; width: 179px; margin-top: 12px; margin-right: 46px;
+			 background: #F2C94C; border-radius: 5px; font-weight: 500; font-size: 14px;
+			 padding: 12px; line-height: 16px;">Free Trial</a>
+		</div>
+	</div>
+</header>
---
+++ № 957/sites.php
@@ -0,0 +1,34 @@
+<div class="sites">
+	<div class="container-fluid" style="height: 266px; background: #F2C94C;">
+		<div class="container" >
+			<div class="row justify-content-center">
+				<h1 style="font-size: 24px; line-height: 34px; font-weight: 500;
+				 margin-top: 35px">Trusted by the World's Best Companies</h1>
+			</div>
+			<div class="row justify-content-center d-flex">
+				<div class="nav" style="height: 80px; max-width: 681px; padding: 0; margin-top: 32px;">
+
+					<a href="" id="a_style" class="nav-link" style="margin-left: 0;">
+						<img class="img-fluid" src="assets/Facebook.png" alt="FB">
+					</a>
+					<a href="" id="a_style" class="nav-link" style="margin-left: 40px;">
+						<img class="img-fluid" src="assets/Twitter.png" alt="Twit">
+					</a>
+					<a href="https://vk.com/avion_music" id="a_style" class="nav-link" style="margin-left: 40px">
+						<img class="img-fluid" src="assets/VK.png" alt="VK">
+					</a>
+					<a href="" id="a_style" class="nav-link a_cl" style="margin-left: 40px;">
+						<img class="img-fluid" src="assets/Instagramm.png" alt="Inst">
+					</a>
+					<a href="" id="a_style" class="nav-link a_cl" style="margin-left: 40px;">
+						<img class="img-fluid" src="assets/Bitrix24.png" alt="Bitrix">
+					</a>
+					<a href="" id="a_style" class="nav-link a_cl" style="margin-left: 40px;">
+						<img class="img-fluid" src="assets/Avito.png" alt="Avito">
+					</a>
+
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
